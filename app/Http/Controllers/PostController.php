962e739e7b267a1e0246f4b81d702b4e637b10f5
Arjest <armand.jeanstephane@gmail.com>
2024-01-21 20:10:31 -0500

    lien tag

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\view\View;
 
@@ -20,9 +21,19 @@ class PostController extends Controller
     public function postsByCategory(Category $category):view
     {
         return view('posts.index',[
-            //'posts' => $category->posts()->paginate(10),
+            //'posts' => $category->posts()->latest()->paginate(10),
             'posts' => Post::where(
                 'category_id', $category->id
+            )->latest()->paginate(10),    
+        ]);
+    }
+
+    public function postsByTag(Tag $tag):view
+    {
+        return view('posts.index',[
+            //'posts' => $tag->posts()->latest()->paginate(10),
+            'posts' => Post::whereRelation(
+                'tags', 'tags.id', $tag->id
             )->latest()->paginate(10),    
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'blog'])->name('blog');
 Route::get('/categories/{category}', [PostController::class, 'postsByCategory'])->name('posts.byCategory');
+Route::get('/tags/{tag}', [PostController::class, 'postsByTag'])->name('posts.byTag');
 Route::get('/{post}', [PostController::class,'show'])->name('posts.show');
