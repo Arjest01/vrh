a9c64237c5ca2ba4bcea364044e946586e75e5cf
Arjest <armand.jeanstephane@gmail.com>
2024-02-13 04:46:33 -0500

    traitement formulaire

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,12 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
     protected $with = [
         'category', 
         'tags'
